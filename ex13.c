a5300057e16d2efbd78996e8db356dcf659634d0
Inahcauo01 <112906776+Inahcauo01@users.noreply.github.com>
2022-09-07 15:29:42 +0100

    Add files via upload

---
+++ ex13.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	int n;
+	printf("entrer le nombre en decimal : ");	scanf("%d",&n);
+	printf("\nEn Octadecimal %o \nEn Hexadecimal %X",n,n);
+	
+	
+}
